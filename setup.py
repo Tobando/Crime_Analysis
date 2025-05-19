cc796ac1e539cd2000de1b0baf8804e2297bfca3
Tamara <tobando@umd.edu>
2025-05-19 15:09:11 -0400

    Initial commit with Cookiecutter DS structure, notebook, and data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Crime pattern analysis in Montgomery County using Python',
+    author='Tamara Obando',
+    license='MIT',
+)
